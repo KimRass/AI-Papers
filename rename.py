744a2b005647fd7255e2490f79c6417a837ec50c
KimRass <rmx1000@yonsei.ac.kr>
2024-06-05 08:58:07 +0900

    SPyNet, DCN

--- rename.py
+++ rename.py
@@ -3,17 +3,17 @@ import re
 
 
 def rename(trg_dir):
-    for path in Path(trg_dir).glob("*.pdf"):
+    for path in Path(trg_dir).glob("**/*.pdf"):
         new_name = path.name.lower()
         new_name = re.sub(r"[ -]", "_", new_name)
-        new_name = re.sub(r"[:]", "", new_name)
+        # new_name = re.sub(r"[:]", "", new_name)
         new_name = re.sub(r'_+', '_', new_name)
 
         path.rename(path.parent/new_name)
 
 
 def main():
-    rename("./")
+    rename(Path(__file__).resolve().parent)
 
 
 if __name__ == "__main__":
