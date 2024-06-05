253a619ada6b5bdbfb3771d70d2fc15c1ccf26c2
KimRass <rmx1000@yonsei.ac.kr>
2024-06-05 09:14:07 +0900

    Renamed

--- rename.py
+++ rename.py
@@ -3,13 +3,16 @@ import re
 
 
 def rename(trg_dir):
-    for path in Path(trg_dir).glob("**/*.pdf"):
-        new_name = path.name.lower()
+    for old_path in Path(trg_dir).glob("**/*.pdf"):
+        old_name = old_path.name
+        new_name = old_name.lower()
         new_name = re.sub(r"[ -]", "_", new_name)
-        # new_name = re.sub(r"[:]", "", new_name)
-        new_name = re.sub(r'_+', '_', new_name)
+        new_name = re.sub(r"[\r\n]+", "_", new_name)
+        new_name = re.sub(r"_+", "_", new_name)
 
-        path.rename(path.parent/new_name)
+        if new_name != old_name:
+            print(f"'{old_name}'\n -> '{new_name}'")
+            old_path.rename(old_path.parent/new_name)
 
 
 def main():
