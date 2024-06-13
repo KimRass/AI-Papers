0bc847b4ba3c9f89421f64aa9cc23c13ce9a047a
KimRass <rmx1000@yonsei.ac.kr>
2024-06-13 09:38:45 +0900

    Added

--- rename.py
+++ rename.py
@@ -3,20 +3,24 @@ import re
 
 
 def rename(trg_dir):
-    for old_path in Path(trg_dir).glob("**/*.pdf"):
-        # old_path = Path("/Users/kimjongbeom/Documents/workspace/AI-Papers/vision/deformable_convnets_v2:_more_deformable,_better_results.pdf")
+    for old_path in Path(trg_dir).glob("**/*"):
+        # old_path=Path("/Users/kimjongbeom/Documents/workspace/AI-Papers/vision/a_style_based_generator_architecture_for_generative_adversarial_networks_pdf")
         old_name = old_path.name
-        new_name = old_name.lower()
-        new_name = re.sub(r"[ -]", "_", new_name)
-        new_name = re.sub(r"[\r\n]+", "_", new_name)
-        new_name = re.sub(r"_+", "_", new_name)
-        new_name = re.sub(r", ", "_", new_name)
-        new_name = re.sub(r",_", "_", new_name)
-        new_name = re.sub(r":_|: |:", "_", new_name)
+        if old_name[-3:] == "pdf":
+            new_name = old_name[:-4]
+            print(new_name)
+            old_path.rename((old_path.parent/new_name).with_suffix(".pdf"))
+    # for old_path in Path(trg_dir).glob("**/*.pdf"):
+    #     old_stem = old_path.stem
+        
+    #     new_stem = old_stem.lower()
+    #     new_stem = re.sub(r"[ -:&]", "_", new_stem)
+    #     new_stem = re.sub(r",[ _]|[\r\n]+", "_", new_stem)
+    #     new_stem = re.sub(r"_+", "_", new_stem)
 
-        if new_name != old_name:
-            print(f"'{old_name}'\n -> '{new_name}'")
-            old_path.rename(old_path.parent/new_name)
+    #     if new_stem != old_stem:
+    #         print(f"'{old_stem}'\n -> '{new_stem}'")
+    #         old_path.rename((old_path.parent/new_stem).with_suffix(".pdf"))
 
 
 def main():
--- vision/basicvsr++.md
+++
@@ -1,9 +0,0 @@
-# Propogation
-
-## Second-Order Grid Propagation
-- BasicVSR은 한 시점, BasicVSR++은 두 시점 이전의 정보를 고려합니다.
-
-# Alignment
-
-# Aggregation & Upsampling
-- Same as BasicVSR (Multiple convolutions and pixel shuffling)
