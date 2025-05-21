f809a3a17f01b0dc6f20cf6eb67043d185e64851
KimRass <rmx1000@yonsei.ac.kr>
2025-05-21 17:13:42 +0900

    Reading Qwen2.5-VL...

--- rename.py
+++ rename.py
@@ -15,14 +15,16 @@ def rename(trg_dir):
         new_stem = re.sub(r"&", "_", new_stem)
         new_stem = re.sub(r":_|: |:", "_", new_stem)
         new_stem = re.sub(r"_+", "_", new_stem)
-        new_path = (old_path.parent/new_stem).with_suffix(".pdf")
+        new_path = (old_path.parent / new_stem).with_suffix(".pdf")
 
         if new_stem != old_stem:
             if new_path.exists():
                 print(f"'{new_stem} already exists!'")
             print(f"[{num}] '{old_stem}'\n    -> '{new_stem}'")
             num += 1
-            old_path.rename((old_path.parent/new_stem).with_suffix(".pdf"))
+            old_path.rename(
+                f"{str(old_path.parent / new_stem)}.pdf"
+            )
 
 
 def main():
